dde41e666f2ee5625107ec839c3041f728b21fe9
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-01-23 12:57:44 -0300

    Retirando a conexao mongo

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,7 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect('mongodb+srv://takao:omnistack@cluster0-h5typ.mongodb.net/waze-dev?retryWrites=true&w=majority', {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
-})
+
 
 app.use(cors());
 
